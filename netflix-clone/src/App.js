7f7af7c301d727a2d51039fc146735d6f26b20c4
Walisson Rodrigues <contact@walisson.com>
2020-09-01 18:27:46 +1000

    Netflix movies rows list. Row component gets data from TMDB API using Axios.

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+import Row from "./Row";
+import requests from "./requests";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hey Wally! </h1>
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Raterd" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
