a5dba1bfd9a38922c85e71ec7657db142c59663c
Walisson Rodrigues <contact@walisson.com>
2020-09-01 18:50:59 +1000

    Row posters positioning and sizing

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -6,11 +6,11 @@ import requests from "./requests";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hey Wally! </h1>
+    <div className="app">
       <Row
         title="Netflix Originals"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Raterd" fetchUrl={requests.fetchTopRated} />
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -4,7 +4,7 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   // A snippet of code which runs based on a specific condition/variable
@@ -17,6 +17,8 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]); // If [],  run once when the row loads, and don't run again
 
+  console.log(movies);
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -25,8 +27,10 @@ function Row({ title, fetchUrl }) {
         {movies.map(movie => (
           <img
             key={movie.id}
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
